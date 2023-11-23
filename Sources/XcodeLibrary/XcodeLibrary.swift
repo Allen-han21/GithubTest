7dc2e549f925bb533bb634fe80cfb27ec9fd7486
allen <allen.han@kidsnote.com>
2023-11-23 10:48:14 +0900

    test function

--- Sources/XcodeLibrary/XcodeLibrary.swift
+++ Sources/XcodeLibrary/XcodeLibrary.swift
@@ -1,2 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct XcodeLibrary {
+    public private(set) var text = "Hello, World!"
+    
+    public init() {
+        
+    }
+    
+    public func foo() {
+        print("Test Complete!")
+    }
+}
